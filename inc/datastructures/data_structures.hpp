9dcfedb1abf60673b6730c59428d1d39bce37d49
leoagea <leoagea@gmail.com>
2025-08-06 17:36:23 +0200

    feat: add data_structures header and update libftpp to include it

--- inc/datastructures/data_structures.hpp
+++ inc/datastructures/data_structures.hpp
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftpp.hpp                                        :+:      :+:    :+:   */
+/*   data_structures.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/05 16:25:17 by lagea             #+#    #+#             */
-/*   Updated: 2025/08/06 17:36:00 by lagea            ###   ########.fr       */
+/*   Created: 2025/08/06 17:35:28 by lagea             #+#    #+#             */
+/*   Updated: 2025/08/06 17:35:35 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPP_HPP
-# define LIBFTPP_HPP
+#ifndef DATA_STRUCTURES_HPP
+# define DATA_STRUCTURES_HPP
 
-#include "datastructures/data_structures.hpp"
+#include "datastructures/data_buffer.hpp"
+#include "datastructures/pool.hpp"
 
 #endif
